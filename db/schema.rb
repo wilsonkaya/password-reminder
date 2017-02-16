b828d44f18342308c128409ee1a886f3706e5975
“wilsonkaya” <fatihkaya2@yahoo.com>
2017-02-16 18:39:29 -0500

    Add null false to accounts paramaters

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2) do
+ActiveRecord::Schema.define(version: 20170215211432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "accounts", force: :cascade do |t|
+    t.string   "web_name",      null: false
+    t.string   "password_hint", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
   create_table "examples", force: :cascade do |t|
     t.text     "text",       null: false
