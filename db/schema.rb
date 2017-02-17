6032ee8e01e3f03620e383d957e19c62bbd43789
“wilsonkaya” <fatihkaya2@yahoo.com>
2017-02-17 09:53:01 -0500

    Create dependency between users and accounts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170215211432) do
+ActiveRecord::Schema.define(version: 20170217142143) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 20170215211432) do
     t.string   "password_hint", null: false
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_accounts_on_user_id", using: :btree
   end
 
   create_table "examples", force: :cascade do |t|
@@ -40,5 +42,6 @@ ActiveRecord::Schema.define(version: 20170215211432) do
     t.index ["token"], name: "index_users_on_token", unique: true, using: :btree
   end
 
+  add_foreign_key "accounts", "users"
   add_foreign_key "examples", "users"
 end
