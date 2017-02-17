5f40caa427e384a85391e16f60b8a2510c6c4c96
“wilsonkaya” <fatihkaya2@yahoo.com>
2017-02-17 10:51:12 -0500

    Add token for update and necessary params

--- scripts/accounts/update.sh
+++ scripts/accounts/update.sh
@@ -5,13 +5,15 @@ URL_PATH="/accounts/${ID}"
 curl "${API}${URL_PATH}" \
   --include \
   --request PATCH \
+  --header "Authorization: Token token=$TOKEN" \
     --header "Content-Type: application/json" \
     --data '{
       "account":{
         "web_name": "'"${WEB_NAME}"'",
-        "password_hint": "'"${PASSWORD_HINT}"'"
+        "password_hint": "'"${PASSWORD_HINT}"'",
+        "user_id": "'"${USER_ID}"'"
       }
     }'
-  # --header "Authorization: Token token=$TOKEN"
+
 
 echo
